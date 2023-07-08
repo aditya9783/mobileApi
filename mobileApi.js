79e26a7bbf33c79c9d11e304f13e9fa534dea418
aditya9783 <adityagupta199818@gmail.com>
2023-07-08 16:46:36 +0530

    Node Modules updated

--- mobileApi.js
+++ mobileApi.js
@@ -13,7 +13,7 @@ app.use(function (req, res, next) {
   );
   next();
 });
-const port = process.env.PORT || 2410
+let port = process.env.PORT || 2410
 app.listen(port, () => console.log(`Node app listening on port ${port}!`));
 let mysql=require("mysql");
 let {mobiles}=require("./mobileData");
